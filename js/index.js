e03555436ce5d2f15b83e5656e448c1b7aada4c8
alemorales <smorales@paloblanco.com>
2023-05-17 09:52:45 -0600

    changes

--- js/index.js
+++ js/index.js
@@ -1,29 +1,60 @@
-
 var mouseX, mouseY;
 
-function setCoords(e) {
-    if (e.offsetX) {
-        mouseX = e.offsetX;
-        mouseY = e.offsetY;
-    } else if (e.layerX) {
-        mouseX = e.layerX;
-        mouseY = e.layerY;
-    }
+function viewCoords(coordenadas) {
+  if (coordenadas.offsetX) {
+    mouseX = coordenadas.offsetX;
+    mouseY = coordenadas.offsetY;
+  } else if (coordenadas.layerX) {
+    mouseX = coordenadas.layerX;
+    mouseY = coordenadas.layerY;
+  }
 
-    document.getElementById('coords').innerHTML = mouseX + ' X' + ', ' + mouseY + ' Y';
+  document.getElementById("coords").innerHTML =
+    mouseX + " X" + ", " + mouseY + " Y";
 }
+
+function setCoords(coordenadas){
+    if (coordenadas.offsetX) {
+        mouseX = coordenadas.offsetX;
+        mouseY = coordenadas.offsetY;
+      } else if (coordenadas.layerX) {
+        mouseX = coordenadas.layerX;
+        mouseY = coordenadas.layerY;
+      }
+      console.log(`${mouseX},${mouseY}`);
+
+      const container_inputs_polygon = document.getElementById("container-inputs-polygon");
+      
+
+      return `${mouseX},${mouseY}`;
+}
+
+
+
+const containerItemImg = document.querySelector(".container-item-img");
+
+console.log(containerItemImg);
+console.log(containerItemImg.offsetHeight);
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
 
 const svgprint = document.getElementById("sev-print-dom");
 var newArray = [];
 
-const valorPoligono1 = '451,227 463,373  159,396 155,391 144,251 149,248 450,224';
-newArray = `
-<polygon class="imp-shape imp-shape-poly" data-shape-title="Casa 69" points="${valorPoligono1}" />
-`;
+fetch("http://localhost:3001/api/v1/coordenadas")
+  .then((response) => response.json())
+  .then((data) => {
+    getData(data);
+  });
 
-svgprint.innerHTML = newArray;
-
-const containerItemImg = document.querySelector('.container-item-img');
-
-console.log(containerItemImg)
-console.log(containerItemImg.offsetHeight)
+function getData(data) {
+  data.forEach((element) => {
+    newArray += `
+        <polygon class="imp-shape imp-shape-poly" data-shape-title="Casa 69" points="${element.secuencia_coordenadas}" />
+        `;
+  });
+  svgprint.innerHTML = newArray;
+}
